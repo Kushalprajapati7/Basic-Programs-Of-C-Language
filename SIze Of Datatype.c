adaf5a2bba74b3dde36f6fbd2e2894760fa96a5d
Kushal Prajapati <87795483+Kushalprajapati7@users.noreply.github.com>
2021-08-28 11:24:38 +0530

    Update SIze Of Datatype.c

--- SIze Of Datatype.c
+++ SIze Of Datatype.c
@@ -7,9 +7,9 @@ int main()
     char charType;
 	
 	printf("\n Size of char: %d byte \n", sizeof(charType));
-    printf("\n Size of int :- %d bytes \n", sizeof(intType));
+    	printf("\n Size of int :- %d bytes \n", sizeof(intType));
  	printf("\n Size of float: %d bytes \n", sizeof(floatType));  
-    printf("\n Size of double: %d bytes \n", sizeof(doubleType));
+    	printf("\n Size of double: %d bytes \n", sizeof(doubleType));
     
     
     return 0;
