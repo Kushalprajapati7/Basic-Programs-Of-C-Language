6e778a7619316d192c9c6df52504fa90ccb9e0fb
Kushal Prajapati <87795483+Kushalprajapati7@users.noreply.github.com>
2021-08-31 18:14:40 +0530

    Add files via upload

---
+++ 007_Area Of Rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int length=07;
+	int width=17;
+	printf("\n  Enter The Value Of n :- \n");
+	scanf("%d",&length);
+	
+	printf("\n  Enter The Value Of n :- \n");
+	scanf("%d",&width);
+	
+	printf("THE AREA OF RECTANGLE IS:--->%d",length*width);
+	
+	return 0;
+}
