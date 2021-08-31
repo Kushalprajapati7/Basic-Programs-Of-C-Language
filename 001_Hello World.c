5d03dc020cb677ced39924d30545aef9d43405f7
Kushal Prajapati <87795483+Kushalprajapati7@users.noreply.github.com>
2021-08-31 12:40:45 +0530

    Add files via upload

---
+++ 001_Hello World.c
@@ -0,0 +1,8 @@
+//hello world program
+#include<stdio.h>
+int main()
+{
+	printf("HELLO WORLD !");
+	return 0;
+}
+
---
+++ 003_Add Two Integers_02(Values Entered By User).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b,c;
+	
+	printf("\n  ENTER THE VALUE OF A:- ",a);
+	scanf("%d",&a);
+	
+	printf("\n  ENTRE THE VALUE OF B:- ",b);
+	scanf("%d",&b);
+	
+	c= a+b;
+	
+	printf("\n  THE SUM OF %d and %d is = %d",a,b,c);
+	
+	return 0;
+}
